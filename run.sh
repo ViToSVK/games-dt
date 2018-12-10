627ad7e8f38aa8bb416ca95d84a69633ddff9aba
Viktor Toman <vtoman07@gmail.com>
2018-12-10 16:10:11 +0100

    stopped tracking binaries

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 JVM_OPTS="-ea -Xmx8g"
-CLASS_PATH="-cp bin/production/games-dt:lib/javabdd-1.0b2.jar"
+CLASS_PATH="-cp out/production/games-dt:lib/javabdd-1.0b2.jar"
 MAIN="main.Main"
 
 java $JVM_OPTS $CLASS_PATH $MAIN $1
